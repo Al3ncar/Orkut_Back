e2c841b6d410f50defddb43ed64973e3d4c756a2
Al3ncar <alencar29htcss@gmail.com>
2026-04-13 03:55:28 -0300

    feat: added new logic valid, added new router and new middlewares

--- src/app.js
+++ src/app.js
@@ -1,16 +1,48 @@
+require("dotenv").config();
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const pool = require("./config/db");
 const app = express();
+const validPost = require("./valid/post");
+const validUser = require("./valid/user");
+const auth = require("./auth/auth")
+
 app.use(express.json());
 
+const formattedDataLocal = (data) => {
+  const date = new Date(data).toLocaleString("pt-BR", {
+    timeZone: "America/Sao_Paulo",
+  });
+  return date;
+};
+
+const resFormatted = (dataRows) => {
+  return dataRows.map((post) => ({
+    ...post,
+    created_by: formattedDataLocal(post.created_by),
+  }));
+};
+
 app.get("/", (req, res) => {
-  res.send("<h1>Olá redes sociais</h1>");
+  res.send("<h1>Olá redes sociaigs</h1>");
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const getUserData = await pool.query(`
+            SELECT * FROM tb_user
+        `);
+    res.status(201).json(resFormatted(getUserData.rows));
+  } catch (err) {
+    res.status(500).json({ err: "Não foi possivel carregar as informações" });
+  }
 });
 
 app.get("/posts", async (req, res) => {
   try {
     const getData = await pool.query(`
             SELECT
+                tb_post.id,
                 tb_user.name AS nome,
                 tb_post.title AS titulo,
                 tb_post.content AS conteudo,
@@ -20,44 +52,79 @@ app.get("/posts", async (req, res) => {
             ORDER BY tb_post.created_by
         `);
 
-    res.json(getData.rows);
+    res.json(resFormatted(getData.rows));
   } catch (err) {
     res.status(500).json({ erro: "FALHA AO FAZER A REQUISIÇÃO" });
   }
 });
 
-app.post("/post", async (req, res) => {
+app.post("/user", validUser, async (req, res) => {
   try {
-    const { user_id, title, content } = req.body;
+    const { name, email, password } = req.body;
     const sendData = await pool.query(
       `
-            INSERT INTO tb_post(user_id, title, content)
+            INSERT INTO tb_user(name, email, password)
             VALUES($1, $2, $3) RETURNING *
         `,
-      [user_id, title, content],
+      [name, email, password],
     );
 
     res.status(201).json({
       mensage: "POST enviado com sucesso!!!",
-      data: sendData.rows[0],
+      data: resFormatted(sendData.rows),
     });
   } catch (err) {
     res.status(500).json({ err: "Não foi possivel criar o post" });
   }
 });
 
-app.get("/user", async (req, res) => {
+app.post("/post", auth, validPost, async (req, res) => {
   try {
-    const getUserData = await pool.query(`
-            SELECT * FROM tb_user
-        `);
-    res.status(201).json(getUserData.rows);
+    const {title, content } = req.body;
+    const sendData = await pool.query(
+      `
+            INSERT INTO tb_post(user_id, title, content)
+            VALUES($1, $2, $3) RETURNING *
+        `,
+      [req.user.id, title, content],
+    );
+    res.status(201).json({
+      mensage: "POST enviado com sucesso!!!",
+      data: resFormatted(sendData.rows),
+    });
   } catch (err) {
-    res.status(500).json({ err: "Não foi possivel carregar as informações" });
+    res.status(500).json({ err: "Não foi possivel criar o post" });
+  }
+});
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await pool.query(
+      `
+        SELECT * FROM tb_user WHERE email= $1
+    `,
+      [email],
+    );
+
+    if (user.rows.length === 0) {
+      return res.status(400).json({ message: "usuario não encontrado" });
+    }
+    if (password != user.rows[0].password) {
+      return res.status(400).json({ message: "Senha Invalida" });
+    }
+
+    const token = jwt.sign({ id: user.rows[0].id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    console.log("login secreto", token);
+
+    res.status(200).json({ token });
+  } catch (err) {
+    console.log(err);
   }
 });
 
-app.put("/post/:id", async (req, res) => {
+app.put("/post/:id", validPost, async (req, res) => {
   try {
     const { id } = req.params;
     const { title, content } = req.body;
@@ -70,11 +137,43 @@ app.put("/post/:id", async (req, res) => {
     );
 
     res.json({
-        mensagem: "Post atualizado com sucesso",
-        data: updatedValue.rows[0]
+      mensagem: "Post atualizado com sucesso",
+      data: resFormatted(updatedValue.rows),
     });
   } catch (err) {
     res.status(500).json({ err: "Não foi possivel atualizar as informações" });
+  }
+});
+
+app.delete("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteData = await pool.query(
+      `DELETE FROM tb_post WHERE id=$1 RETURNING *`,
+      [id],
+    );
+    res.json({
+      msg: "Item excluido com sucesso",
+      data: resFormatted(deleteData.rows[0]),
+    });
+  } catch (err) {
+    res.status(500).json({ err: "Não foi possivel deletar as informações" });
+  }
+});
+
+app.delete("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteData = await pool.query(
+      `DELETE FROM tb_user WHERE id=$1 RETURNING *`,
+      [id],
+    );
+    res.json({
+      msg: "Item excluido com sucesso",
+      data: resFormatted(deleteData.rows[0]),
+    });
+  } catch (err) {
+    res.status(500).json({ err: "Não foi possivel deletar as informações" });
   }
 });
 
